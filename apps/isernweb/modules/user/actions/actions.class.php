6abcbaf2f7caefcc2d128231f0c0933d7914679f
kenglish <kenglish@2145ded1-b23f-0410-ad62-bfe817b7ab27>
2007-11-26 09:17:46 +0000

    new modules

---
+++ apps/isernweb/modules/user/actions/actions.class.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * user actions.
+ *
+ * @package    myisern-lime
+ * @subpackage user
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
+ */
+class userActions extends sfActions
+{
+  /**
+   * Executes index action
+   *
+   */
+  public function executeIndex()
+  {
+    $this->forward('default', 'module');
+  }
+  
+  
+  public function executeLogin()
+  {
+   if ($this->getRequest()->getMethod() != sfRequest::POST)
+   {
+     // display the form
+     $this->getRequest()->setAttribute('referer', $this->getRequest()->getReferer());
+   }
+   else
+   {
+//     // handle the form submission
+//     $login = $this->getRequestParameter('login');
+// 
+//     $c = new Criteria();
+//     $c->add(UserPeer::LOGIN, $login);
+//     $user = UserPeer::doSelectOne($c);
+// 
+//     // nickname exists?
+//     if ($user)
+//     {
+//     	$this->getUser()->setAuthenticated(true);
+     	return $this->redirect('main');
+     
+//     } else {
+//     	return sfView::SUCCESS; 
+//     }
+   }
+  }  
+  public function executeLogout()
+  {
+    $this->getUser()->setAuthenticated(false);
+    $this->getUser()->clearCredentials();
+ 
+    $this->redirect('@homepage');
+  }
+  public function handleErrorLogin()
+  {
+    return sfView::SUCCESS;
+  }
+}
---
+++ apps/isernweb/modules/user/templates/loginSuccess.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Created on Nov 26, 2007
+ *
+ * IsernWeb, Developed by Kevin English for ICS 613
+ */
+?>
+<?php if ($sf_request->hasErrors()): ?>  
+  <div id="errors" style="padding:10px;">
+    Please correct the following errors and resubmit:
+    <ul>
+    <?php foreach ($sf_request->getErrors() as $error): ?>
+      <li><?php echo $error ?></li>
+    <?php endforeach; ?>
+    </ul>
+  </div>
+<?php endif; ?>
+
+<?php echo form_tag('user/login') ?>
+
+  
+ <table>
+<tbody>
+<tr>
+  <th>Login:</th>
+  <td><?php echo input_tag('login', $sf_params->get('login')) ?></td>
+</tr>
+<tr>
+  <th>Password:</th>
+  <td><?php echo input_password_tag('password') ?></td>
+</tr>
+<tr>
+  <td colspan=2>  <?php echo input_hidden_tag('referer', $sf_request->getAttribute('referer')) ?>
+  <?php echo submit_tag('sign in') ?></td>
+</tr>
+ 
+
+</tbody>
+</table>  
+  
+
+ 
+  
+ 
+
+</form>
+ 
