9215dd4b7cf371485f6f385b9c32c5f945f00c6e
kenglish <kenglish@2145ded1-b23f-0410-ad62-bfe817b7ab27>
2007-12-01 19:40:46 +0000

    added another test to login validator

--- trunk/test/unit/myLoginValidatorTest.php
+++ trunk/test/unit/myLoginValidatorTest.php
@@ -5,58 +5,55 @@ require(dirname(__FILE__).'/../../apps/isernweb/lib/myUser.class.php');
 require(dirname(__FILE__).'/../../apps/isernweb/lib/myLoginValidator.class.php');
 require(dirname(__FILE__).'/../../plugins/sfPropelTestPlugin/bootstrap/propel-unit.php');
 
-    class myUnitTest extends sfPropelTest
+class myLoginValidatorTest extends sfPropelTest
+{
+  public function setup() {
+    $context = sfContext::getInstance();
+    $this->request = $context->getRequest();
+    $this->manager = new sfValidatorManager();
+    $this->manager->initialize($context);
+    $validator = new myLoginValidator();
+    $validator->initialize($context, array('password' => 'password'));
+    $this->manager->registerName('login', false);
+    $this->manager->registerValidator('login', $validator);
+  }
+  public function teardown() {
+    $this->request->removeError('login');
+  }
+  public function test_validator_success()
+  {
+    $this->request->setParameter('login', 'isern');
+    $this->request->setParameter('password', 'isern2008');
+    $retval = $this->manager->execute();
+    $this->is($retval, true, "validator should return true");
+    $this->is($this->request->getErrors(), Array(), "errors should be an empty array");
+  }
+  public function test_validator_failures()
+  {
+
+    $values =  array('invalid', 'testbad');
+    $this->diag('myLoginValidator()');
+    foreach ( $values as $value ) 
     {
+      $this->setup();
+      $this->request->setParameter('login', $value);
+      $this->request->setParameter('password', $value);
+      $retval = $this->manager->execute();
+      $this->is($retval, false, "validator should return false for $value ");
+      $this->ok(sizeof($this->request->getErrors()) > 0, "errors should not be an empty array");      
+      $this->request->removeError('login');
+            
+    } 
+  }
+  public function test_validator_bad_password()
+  {
+    $this->request->setParameter('login', 'isern');
+    $this->request->setParameter('password', 'isern2008xxx');
+    $retval = $this->manager->execute();
+    $this->is($retval, false, "validator should return true");
+    $this->ok(sizeof($this->request->getErrors()) > 0, "errors should not be an empty array");
+  }  
+}
 
-      public function test_validator_success()
-      {
-        $context = sfContext::getInstance();
-        $request = $context->getRequest();
-        $manager = new sfValidatorManager();
-        $manager->initialize($context);
-        $validator = new myLoginValidator();
-        $validator->initialize($context);
-
-        $manager->registerName('myvalidator', false);
-        $manager->registerValidator('myvalidator', $validator);
- 
-        $request->setParameter('login', 'isern');
-        $request->setParameter('password', 'isern2008');
-        $retval = $manager->execute();
-        $this->is($retval, true, "validator should return true");
-        $this->is($request->getErrors(), Array(), "errors should be an empty array");
-        $request->removeError('myvalidator');
-      }
-
-
-      public function test_validator_failures()
-      {
-        $context = sfContext::getInstance();
-        $request = $context->getRequest();
-        $manager = new sfValidatorManager();
-        $manager->initialize($context);
-        $validator = new myLoginValidator();
-        $validator->initialize($context);
-
-//        $values =  array('invalid');
-        $values =  array('invalid', 'testbad');
-        $this->diag('myLoginValidator()');
-        foreach ( $values as $value ) { 
-          $manager->registerName('login', true);
-          $manager->registerValidator('login', $validator);
-          $request->setParameter('login', $value);
-          $request->setParameter('password', $value);
-          $retval = $manager->execute();
-          $this->is($retval, false, "validator should return false for $value ");
-          $request->removeError('myLoginValidator');
-        } 
-#        print_r($request->getErrors()); 
-##        $this->is($request->getErrors(), Array());
-#        $request->removeError('myvalidator');
-      }
-      
-
-    }
-
-$test = new myUnitTest();
+$test = new myLoginValidatorTest();
 $test->execute();
