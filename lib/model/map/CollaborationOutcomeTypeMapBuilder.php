12ad2b6b12a76aa385ee9432d0d0bc05de431ac1
kenglish <kenglish@2145ded1-b23f-0410-ad62-bfe817b7ab27>
2007-12-08 08:54:16 +0000

    new stuff

---
+++ lib/model/map/CollaborationOutcomeTypeMapBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+class CollaborationOutcomeTypeMapBuilder {
+
+	
+	const CLASS_NAME = 'lib.model.map.CollaborationOutcomeTypeMapBuilder';
+
+	
+	private $dbMap;
+
+	
+	public function isBuilt()
+	{
+		return ($this->dbMap !== null);
+	}
+
+	
+	public function getDatabaseMap()
+	{
+		return $this->dbMap;
+	}
+
+	
+	public function doBuild()
+	{
+		$this->dbMap = Propel::getDatabaseMap('propel');
+
+		$tMap = $this->dbMap->addTable('isern_collaboration_outcome_types');
+		$tMap->setPhpName('CollaborationOutcomeType');
+
+		$tMap->setUseIdGenerator(true);
+
+		$tMap->addPrimaryKey('ID', 'Id', 'int', CreoleTypes::INTEGER, true, null);
+
+		$tMap->addForeignKey('COLLABORATION_ID', 'CollaborationId', 'int', CreoleTypes::INTEGER, 'isern_collaborations', 'ID', false, null);
+
+		$tMap->addColumn('OUTCOME_TYPE', 'OutcomeType', 'string', CreoleTypes::VARCHAR, false, 50);
+
+	} 
+} 
---
+++ lib/model/map/OrganizationResearcherKeywordMapBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+class OrganizationResearcherKeywordMapBuilder {
+
+	
+	const CLASS_NAME = 'lib.model.map.OrganizationResearcherKeywordMapBuilder';
+
+	
+	private $dbMap;
+
+	
+	public function isBuilt()
+	{
+		return ($this->dbMap !== null);
+	}
+
+	
+	public function getDatabaseMap()
+	{
+		return $this->dbMap;
+	}
+
+	
+	public function doBuild()
+	{
+		$this->dbMap = Propel::getDatabaseMap('propel');
+
+		$tMap = $this->dbMap->addTable('isern_organization_researcher_keywords');
+		$tMap->setPhpName('OrganizationResearcherKeyword');
+
+		$tMap->setUseIdGenerator(true);
+
+		$tMap->addPrimaryKey('ID', 'Id', 'int', CreoleTypes::INTEGER, true, null);
+
+		$tMap->addForeignKey('ORGANIZATION_ID', 'OrganizationId', 'int', CreoleTypes::INTEGER, 'isern_organizations', 'ID', false, null);
+
+		$tMap->addColumn('KEYWORD', 'Keyword', 'string', CreoleTypes::VARCHAR, false, 50);
+
+	} 
+} 
