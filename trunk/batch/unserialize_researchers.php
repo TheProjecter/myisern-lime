2ccd91c7bf6a510cfd774a4ee4a2f430938dd154
kenglish <kenglish@2145ded1-b23f-0410-ad62-bfe817b7ab27>
2007-12-08 08:52:16 +0000

    batch files used to load the xml

---
+++ trunk/batch/unserialize_researchers.php
@@ -0,0 +1,39 @@
+<?
+require_once 'XML/Unserializer.php'; 
+// Include XML_Unserializer
+require_once 'XML/Unserializer.php';
+
+$file = 'researchers.xml'; 
+
+$handle = fopen($file,'r');
+
+$doc = fread($handle, filesize($file));
+
+fclose($handle);
+
+// Instantiate the serializer
+$Unserializer = &new XML_Unserializer();
+
+// Serialize the data structure
+$status = $Unserializer->unserialize($doc);
+
+// Check whether serialization worked
+if (PEAR::isError($status)) {
+   die($status->getMessage());
+}
+
+// Display the PHP data structure
+#var_dump($Unserializer->getUnserializedData());
+$data = $Unserializer->getUnserializedData(); 
+foreach ($data['Researcher'] as $i => $row) { 
+  $j=$i+1;
+  echo "  r$j:\n";
+  echo "    name: " . $row['Name'] . "\n";
+  echo "    organization: " . $row['Organization'] . "\n";
+  echo "    email: " . $row['Email'] . "\n";
+  echo "    picture_link: " . $row['Picture-Link'] . "\n";
+  echo "    bio_statement: " . $row['Bio-Statement-Link'] . "\n";
+
+} 
+
+?>
