3c23cd4a4cf49c601c2b6bc13ffa5d15169fa75d
kenglish <kenglish@2145ded1-b23f-0410-ad62-bfe817b7ab27>
2007-12-08 08:54:16 +0000

    new stuff

---
+++ trunk/lib/model/map/CollaborationTypeMapBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+class CollaborationTypeMapBuilder {
+
+	
+	const CLASS_NAME = 'lib.model.map.CollaborationTypeMapBuilder';
+
+	
+	private $dbMap;
+
+	
+	public function isBuilt()
+	{
+		return ($this->dbMap !== null);
+	}
+
+	
+	public function getDatabaseMap()
+	{
+		return $this->dbMap;
+	}
+
+	
+	public function doBuild()
+	{
+		$this->dbMap = Propel::getDatabaseMap('propel');
+
+		$tMap = $this->dbMap->addTable('isern_collaboration_types');
+		$tMap->setPhpName('CollaborationType');
+
+		$tMap->setUseIdGenerator(true);
+
+		$tMap->addPrimaryKey('ID', 'Id', 'int', CreoleTypes::INTEGER, true, null);
+
+		$tMap->addForeignKey('COLLABORATION_ID', 'CollaborationId', 'int', CreoleTypes::INTEGER, 'isern_collaborations', 'ID', false, null);
+
+		$tMap->addColumn('COLLABORATION_TYPE', 'CollaborationType', 'string', CreoleTypes::VARCHAR, false, 50);
+
+	} 
+} 
